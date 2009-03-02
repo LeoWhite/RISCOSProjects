e311b54343fa9d923a05d80644eb301b83a9df02
leo <leo.white@mybigideas.co.uk>
2009-03-02 23:50:03 +0000

    Changed software page to be auto generated from a data base

---
+++ WebPages/html/software.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+</head>
+<body>
+
+<?php
+$link = @mysql_connect("db1748.oneandone.co.uk", "dbo274106622", "5MqwnprJ") or
+die(mysql_error());
+
+if(!mysql_select_db("db274106622", $link)) {
+  echo("Failed to open database");
+}
+?>
+
+  <h1>Software 'Wot I have written</h1>
+
+  <p>The following programs are written for computers running <a href="http://www.riscos.com">RISC OS<a/>. These can either be run on a real 
+    piece of RISC OS hardware, or on one of its emulators.</a>
+  </p>
+
+<?php
+$result = mysql_query("SELECT * FROM Software", $link);
+
+while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo("<dl>");
+    echo("<dt><a href=\"" . $row["Location"] . "\">" . $row["Name"] . "</a></dt>");
+    echo("<dd>" . $row["Description"] . "</dd>");
+    echo("</dl>");
+}
+ 
+?>
+
+</body>
+
+
+<?php
+mysql_close($link);
+?>
+
+</html>
