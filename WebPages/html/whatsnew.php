0adf625e4781011de21f89f3532e6310ff48018c
leo <leo.white@mybigideas.co.uk>
2012-09-02 20:33:14 +0000

    Added a menu bar

--- WebPages/html/whatsnew.php
+++ WebPages/html/whatsnew.php
@@ -1,8 +1,15 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
+ <link rel="stylesheet" href="../style.css" type="text/css">	
 </head>
 <body>
+
+<div id="TopMenu">
+	<a href="../index.html">Home</a> | <a href="whatsnew.php">What&apos;s New?</a> | <a href="software.php">Software</a> | <a href="https://plus.google.com/photos/107287498294316622336/albums/5758446343165527137">BigTrak</a>
+</div>
+
+<div style="padding: 1em 0px 0px 0px;"></div>
 
 <?php
 $link = @mysql_connect("db1748.oneandone.co.uk", "dbo274106622", "5MqwnprJ") or
