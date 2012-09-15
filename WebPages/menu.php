054adadd7f1ab2798ef06f15414289aabe4c9d33
leo <leo.white@mybigideas.co.uk>
2012-09-15 17:59:39 +0000

    Latest bigtrak changes

--- WebPages/menu.php
+++ WebPages/menu.php
@@ -1,6 +1,6 @@
 <div id="TopMenu">
         <a href="/">Home</a> | <a href="/html/whatsnew.php">What&apos;s New?</a> | <a href="/html/software.php">Software</
-a> | <a href="https://plus.google.com/photos/107287498294316622336/albums/5758446343165527137">BigTrak</a>
+a> | <a href="/RPi/BigTrak/">BigTrak</a>
 </div>
 
 <div style="padding: 4em 0px 0px 0px;"></div>
