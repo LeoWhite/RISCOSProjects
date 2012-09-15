f986b66b89905d6ce48dc00e7fc62ccb146e2a69
leo <leo.white@mybigideas.co.uk>
2012-09-15 17:59:09 +0000

    Latest bigtrak changes

---
+++ WebPages/RPi/BigTrak/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+ <link rel="stylesheet" href="../style.css" type="text/css">	
+ <title>BigTraks and Raspberry Pi&quote;s</title>
+</head>
+<body>
+
+<?php
+include '../menu.php'
+ 
+?>
+
+<h1>Opening up the BigTrak</h1>
+
+<p>On this page I&quote;ve written down the steps I went through to open up my BigTrak. This list is fairly non-destructive, meaning that at the end of it you can reverse the steps and having a fully working BigTrak once more.<br/>
+</p>
+
+<table>
+<tr>
+ <td>Firstly remove the BigTrak from its packaging and check that its working as expected. Easiest way if to insert the batteries and run the &quote;test&quote; program as mentioned in the manual. </td>
+ <td ><a href="images/BigTrak1.jpg"><img src="images/thumbs/BigTrak1.jpg"></a></td>
+</tr>
+
+<tr>
+ <td>At this point the BigTrak looks fairly plain, so I went ahead and applied the provided stickers. This is an optional step and some people may like to leave the BigTrak looking factory fresh, or wish to apply their own decorations.</td>
+ <td><a href="images/BigTrak2.jpg"><img align="right" src="images/thumbs/BigTrak2.jpg"></a><a href="images/BigTrak3.jpg"><img src="images/thumbs/BigTrak3.jpg"></a></td>
+</tr>
+
+<tr>
+ <td>Next flip the Bigtrak upside down and remove the 9 screws from around the edge of the BigTrak (See picture for location). These are mostly easy to get at, however the ones on the front of the BigTrak may require an appropiatly slender screwdriver to reach them.</td>
+ <td><a href="images/BigTrak4.jpg"><img align="right" src="images/thumbs/BigTrak4.jpg"></a></td>
+</tr>
+
+<tr>
+ <td>The last two screws holding the top of the BigTrak in place are located underneath the grey grill on the back. This grill is held in place by four platic tabs which need to be pushed in, whilst at the same time sliding the grill away from BigTrak.  This can be quite tricky, as there&quote;s limited space to get extra hands in to help out. One way I&quote; I&quote;ve gotten the grill off is to use to wedge some case opening tools into the sides, to keep those two tabs unlocked, whilst using my fingers to push in the bottom two tabs and slide the grill upwards.</td>
+ <td><a href="images/BigTrak5.jpg"><img align="right" src="images/thumbs/BigTrak5.jpg"></a></td>
+</tr>
+
+<tr>
+ <td>With the grill removed its then a simple case to remove the last two screws.</td>
+ <td><a href="images/BigTrak6.jpg"><img align="right" src="images/thumbs/BigTrak6.jpg"></a></td>
+</tr>
+
+<tr>
+ <td>Flip the BigTrak back onto its wheels and start to carefully loosen the the top of the BigTrak and lift upwards. the lid is connected to the base with a ribbon cable and a switch, so only pull the lift up far enough to tilt it to oneside and expose the inside of the BigTrak.</td>
+ <td><a href="images/BigTrak7.jpg"><img align="right" src="images/thumbs/BigTrak7.jpg"></a></td>
+</tr>
+
+<tr>
+ <td>With the lid lifted up onto one edge is should now be a simple case to remove the screw holding the switch in place and remove it from the lid. Next unscrew the two screws on the PCB that hold the ribbon cable in place and let it slip free.</td>
+ <td><a href="images/BigTrak8.jpg"><img align="right" src="images/thumbs/BigTrak8.jpg"></a></td>
+</tr>
+
+<tr>
+ <td>With the switch and ribbong cable disconnected the lid will now come free and be completely removed from the base of the BigTrak.</td>
+ <td><a href="images/BigTrak9.jpg"><img align="right" src="images/thumbs/BigTrak9.jpg"></a></td>
+</tr>
+
+</table>
+
+</body>
+
+</html>
